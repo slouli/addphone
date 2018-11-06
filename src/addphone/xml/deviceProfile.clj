1d53c457e149ed7cc0e1a8b2722738863f0423ba
Steve Louli <slouli@opentext.com>
2018-11-06 13:33:58 -0500

    "Added XML Code for addDeviceProfile"
    -Added the code for adding device profiles

--- src/addphone/xml/deviceProfile.clj
+++ src/addphone/xml/deviceProfile.clj
@@ -8,3 +8,34 @@
   {:name "getDeviceProfile"
    :xml (xml/element :ns:getDeviceProfile {:sequence "?"}
                (xml/element :name {} dpName))})
+
+                      
+(defn addDeviceProfile
+  [userId description line pt e164Mask model]
+  (let [models {:8851 "Cisco 8851"
+                :7945 "Cisco 7945"}
+        pbtemplate {:8851 "Standard 8851 SIP"
+                    :7945 "OT 1 Line + Speed Dial - 7945 SIP"}
+        sktemplate {:8851 "OT - Standard User - AbbrDial"
+                    :7945 "OT - Standard User - AbbrDial"}]
+                    
+    {:name "addDeviceProfile" 
+     :xml (xml/element :ns:addDeviceProfile {:sequence "?"}
+            (xml/element :deviceProfile {}                       
+              (xml/element :name {} userId)
+              (xml/element :description {} description)
+              (xml/element :useLocale {} "English United States")
+              (xml/element :product {} ((keyword model) models)) ;;Can be one of "Cisco 7945" "Cisco 8851" "Cisco Unified Services Framework"
+              (xml/element :protocol {} "SIP")
+              (xml/element :protocolSide {} "User")
+              (xml/element :phoneTemplateName {} ((keyword model) pbtemplate))
+              (xml/element :softkeyTemplateName {} ((keyword model) sktemplate))
+              (xml/element :lines {}
+                (xml/element :line {}
+                  (xml/element :dirn {}
+                    (xml/element :pattern {} line)
+                    (xml/element :routePartitionName {} pt))
+                  (xml/element :display {} description)
+                  (xml/element :displayAscii {} description)
+                  (xml/element :e164Mask {} e164Mask)))))}))
+
