980f35a6a0e6784b62cab07eecd54cb548b9c84f
Steve Louli <slouli@opentext.com>
2018-11-06 20:45:06 -0500

    Preliminary working version of core.clj
    -Implemented main function that can check for existing users
     and create new device profiles

--- src/addphone/core.clj
+++ src/addphone/core.clj
@@ -9,27 +9,37 @@
 (def americas
   {:ip "10.230.154.5" :ver "10.5"})
 
+;userId description line loc model
 (defn request
   [cluster func & args]
   (let [funcmap (apply func args)
         name (:name funcmap)
         xml (:xml funcmap)]
-  @(client/axl cluster name xml)))
+   @(client/axl cluster name xml)))
 
+(defn echo
+  [msg f & args]
+  (do
+    (print msg)
+    (def result (apply f args))
+    (println result)
+    result))
+  
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "I create the device profile/end user account/jabber accounts for new user onboarding"
   [& args]
-  (let [userExists? (fn [userId] (parse/exists? (request americas user/getUser userId)))
+  (let [argmap (zipmap '(:userId :description :line :loc :model) args)
+        userExists? (fn [userId] (parse/exists? (request americas user/getUser userId)))
         lineExists? (fn [line pt] (parse/exists? (request americas line/getLine line pt)))
         deviceProfileExists? (fn [userId] (parse/exists? (request americas deviceProfile/getDeviceProfile userId)))]
 
-  (defn argmap
-    [args]
-    (let [keys '(:userId :fname :line :location)]
-      (zipmap keys args)))
+    (def proceed?
+       (every? false? (list 
+                        (echo "Checking if user exists... " userExists? (:userId argmap))
+                        (echo "Checking if line exists... " lineExists? (:line argmap) (:loc argmap))
+                        (echo "Checking if device profile exists... " deviceProfileExists? (:userId argmap)))))
+    (cond 
+      (false? proceed?) (println "Cannot proceed, verify the user/profile/extension are not already assigned")
+      :else (println (request americas deviceProfile/addDeviceProfile argmap)))))
 
-  (println (or (userExists? (first args))
-               (lineExists? "1234" "PT-Toronto-Dev")
-               (deviceProfileExists? (first args))))
-  (println (argmap args))))
