06bb03b6ab11eccb5f53b63629e8a2e150079ee1
Steve Louli <slouli@opentext.com>
2018-11-06 20:46:13 -0500

    Completed deviceprofiles XML
    -Add DeviceProfile XML is working for gets and adds
    -Refinement of how e164 addresses should be implemented

--- src/addphone/xml/deviceProfile.clj
+++ src/addphone/xml/deviceProfile.clj
@@ -8,34 +8,38 @@
   {:name "getDeviceProfile"
    :xml (xml/element :ns:getDeviceProfile {:sequence "?"}
                (xml/element :name {} dpName))})
+     
 
-                      
 (defn addDeviceProfile
-  [userId description line pt e164Mask model]
+  [{:keys [userId description line loc model]}]
   (let [models {:8851 "Cisco 8851"
                 :7945 "Cisco 7945"}
         pbtemplate {:8851 "Standard 8851 SIP"
                     :7945 "OT 1 Line + Speed Dial - 7945 SIP"}
         sktemplate {:8851 "OT - Standard User - AbbrDial"
-                    :7945 "OT - Standard User - AbbrDial"}]
-                    
+                    :7945 "OT - Standard User - AbbrDial"}
+        masks {:RichmondHill "90576XXXXX"
+               :Toronto "41695XXXXX"}]
+    
     {:name "addDeviceProfile" 
      :xml (xml/element :ns:addDeviceProfile {:sequence "?"}
             (xml/element :deviceProfile {}                       
               (xml/element :name {} userId)
               (xml/element :description {} description)
-              (xml/element :useLocale {} "English United States")
+              (xml/element :userLocale {} "English United States")
               (xml/element :product {} ((keyword model) models)) ;;Can be one of "Cisco 7945" "Cisco 8851" "Cisco Unified Services Framework"
+              (xml/element :class {} "Device Profile")
               (xml/element :protocol {} "SIP")
               (xml/element :protocolSide {} "User")
               (xml/element :phoneTemplateName {} ((keyword model) pbtemplate))
               (xml/element :softkeyTemplateName {} ((keyword model) sktemplate))
               (xml/element :lines {}
                 (xml/element :line {}
+                  (xml/element :index {} "1")
                   (xml/element :dirn {}
                     (xml/element :pattern {} line)
-                    (xml/element :routePartitionName {} pt))
+                    (xml/element :routePartitionName {} (str "PT-" loc "-Dev")))
                   (xml/element :display {} description)
                   (xml/element :displayAscii {} description)
-                  (xml/element :e164Mask {} e164Mask)))))}))
+                  (xml/element :e164Mask {} ((keyword loc) masks))))))}))
 
